6ac757d1117260d0c32d4ba6a5e47fd00f6e473e
ROGER Equipaciones <rogerjunguitubriz@MacBook-Pro-de-roger-junguitu-briz.local>
2014-07-18 12:36:42 +0200

    it's failing now

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -6,18 +6,19 @@ class ChessValidator
 		@board=board
 	end
 
-	def check_moves(move=false)
+	def check_moves(move = false)
 		if @board.empty?
-			return 'ILLEGAL'
+			return ['ILLEGAL']
 		end
-		'LEGAL'
+		move.map do |from, to|
+			to.split('')[1].to_i - from.split('')[1].to_i > 2 ? 'ILLEGAL' : 'LEGAL'
+		end
 	end
-
 end
 describe "Chess Validator" do
 
 	it "all moves are illegal if there are no pieces" do
-		expect(ChessValidator.new({}).check_moves).to eq('ILLEGAL')
+		expect(ChessValidator.new({}).check_moves).to eq(['ILLEGAL'])
 	end
 
 	it 'the board has one white pawn and moves one step' do
@@ -30,7 +31,7 @@ describe "Chess Validator" do
 			'a2'=>'a3'
 		}
 
-		expect(ChessValidator.new(board).check_moves(moves)).to eq('LEGAL')
+		expect(ChessValidator.new(board).check_moves(moves)).to eq(['LEGAL'])
 	end
 
 	it 'the board has two white pawns and both of them move one step' do
@@ -45,6 +46,51 @@ describe "Chess Validator" do
 			'a2'=>'a3',
 			'b2'=>'b3'
 		}
-		expect(ChessValidator.new(board).check_moves(moves)).to eq('LEGAL')
+		expect(ChessValidator.new(board).check_moves(moves)).to eq(['LEGAL', 'LEGAL'])
 	end
+
+	it 'the board has two pawns and one of them make and illegal move' do
+		board = {
+			'a2'=>'wP',
+			'a3' => '--',
+			'b2'=>'wP',
+			'b3' => '--'
+		}
+
+		moves = {
+			'a2'=>'a5',
+			'b2'=>'b3'
+		}
+		expect(ChessValidator.new(board).check_moves(moves)).to eq(['ILLEGAL', 'LEGAL'])
+	end
+
+	it 'the board has two pawns and both of them make and illegal move' do
+		board = {
+			'a2'=>'wP',
+			'a3' => '--',
+			'b2'=>'wP',
+			'b3' => '--'
+		}
+
+		moves = {
+			'a2'=>'a5',
+			'b3'=>'b5'
+		}
+		expect(ChessValidator.new(board).check_moves(moves)).to eq(['ILLEGAL', 'ILLEGAL'])
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
